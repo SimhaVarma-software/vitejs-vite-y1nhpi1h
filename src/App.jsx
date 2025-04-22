4b8467ce140dda7a650b7566f35071ff24ce782b
SimhaVarma-software <simhavarma79@gmail.com>
2025-04-22 15:34:04 +0530

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
+import Form from './form';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="/add-movie" element={<Form/>} />
       </Routes>
     </Router>
   );
